064ec26bc3739da8985a8034afc3db42e12f274f
SpecCRA <benjamin.y.xiao@gmail.com>
2019-02-07 19:38:34 -0800

    Started psuedocode for spaghetti plot

---
+++ plot_code.py
@@ -0,0 +1,27 @@
+# Plotly code for February submission
+
+"""
+1. Plot all teams besides the Mavs and Rockets in light grey
+2. Create a separate plot for the Mavs and Rockets outside of the loop
+3. Annotations: label min and max points - make dots for each
+"""
+
+# Data for charts
+traces = []
+
+# Loop through teams to append lines
+
+# Dallas Mavs line
+
+# Houston Rockets line
+
+# Max points dot
+# Text: "[TEAM] scored the most points in one game."
+
+# Min points dot
+# Text: "[TEAM] scored the least ponits."
+
+# Most 3-pointers made by a team game
+# Text: "[TEAM] made the most 3-pointers in one game to date."
+
+# Annotations + Title
